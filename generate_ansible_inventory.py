66aaae3c1f7a3d90339d8b987acf1f22101c8f44
ksator <khelil.sator.git@gmail.com>
2018-10-17 12:57:47 +0200

    m

---
+++ generate_ansible_inventory.py
@@ -0,0 +1,30 @@
+#############################################
+# usage:
+# vi variables.yml
+# python ./generate_ansible_inventory.py
+#############################################
+
+import yaml
+from jinja2 import Template
+
+def import_variables_from_file():
+    my_variables_file=open('variables.yml', 'r')
+    my_variables_in_string=my_variables_file.read()
+    my_variables_in_string=my_variables_in_string.replace('device-id', 'device_id')
+    my_variables_in_yaml=yaml.load(my_variables_in_string)
+    my_variables_file.close()
+    return my_variables_in_yaml
+
+def generate_ansible_inventory():
+    f=open('templates/inventory.j2')
+    my_template = Template(f.read())
+    f.close()
+    f=open('hosts','w')
+    f.write(my_template.render(my_variables_in_yaml))
+    f.close()
+    return('done')
+
+my_variables_in_yaml=import_variables_from_file()
+
+generate_ansible_inventory()
+
