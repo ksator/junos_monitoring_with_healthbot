bc11cad86627e8fc63692400d6e84917a12c8e6c
ksator <khelil.sator.git@gmail.com>
2018-11-02 20:53:18 +0100

    m

--- functions/enable_interface.py
+++ functions/enable_interface.py
@@ -2,11 +2,9 @@ from jinja2 import Template
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 
-def enable_interface(dev, int):
-    f=open('functions/enable_interface.j2')
-    my_template = Template(f.read())
-    f.close()
-    device=Device(host=dev, user='root', password='Embe1mpls')
+def enable_interface(int):
+    my_template = Template('delete interfaces {{ interface }} disable')
+    device=Device(host='10.49.102.160', user='root', password='Embe1mpls')
     device.open()
     cfg=Config(device)
     cfg.load(my_template.render(interface = int), format='set')
@@ -14,4 +12,5 @@ def enable_interface(dev, int):
     device.close()
 
 # enable_interface('10.49.102.160', 'ge-0/0/5')
+
 
