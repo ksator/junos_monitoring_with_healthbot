38aa035bf43d4d928667f2e4ba188d433fe81f0e
ksator <khelil.sator.git@gmail.com>
2018-11-06 17:01:37 +0100

    m

--- functions/enable_interface.py
+++ functions/enable_interface.py
@@ -1,16 +1,31 @@
 from jinja2 import Template
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
+import requests
+from requests.auth import HTTPBasicAuth
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-def enable_interface(int):
-    my_template = Template('delete interfaces {{ interface }} disable')
-    device=Device(host='10.49.102.160', user='root', password='Embe1mpls')
-    device.open()
-    cfg=Config(device)
-    cfg.load(my_template.render(interface = int), format='set')
-    cfg.commit()
-    device.close()
+def get_junos_details(dev):
+	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+	healthbot_user = 'root'
+	healthbot_pwd = 'jfit'
+	healthbot_server = '10.209.18.74'
+	headers = { 'Accept' : 'application/json', 'Content-Type' : 'application/json' }
+	url = 'https://'+ healthbot_server + ':8080/api/v1/device/device_id/'
+	r = requests.get(url, auth=HTTPBasicAuth(healthbot_user, healthbot_pwd), headers=headers, verify=False)
+	return r.json()
 
-# enable_interface('10.49.102.160', 'ge-0/0/5')
-
+def enable_interface(dev, int):
+	junos_details = get_junos_details(dev)
+	junos_host = junos_details['host']
+	junos_user = junos_details['authentication']['password']['username']
+	# junos_password = junos_details['authentication']['password']['password']
+	junos_password = 'MaRtInI'
+	device=Device(host=junos_host, user=junos_user, password=junos_password)
+	device.open()
+	cfg=Config(device)
+	my_template = Template('delete interfaces {{ interface }} disable')
+	cfg.load(my_template.render(interface = int), format='set')
+	cfg.commit()
+	device.close()
 
