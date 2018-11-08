cf99f938b412be8bf2241fd1b6c13576f40cfcfa
ksator <khelil.sator.git@gmail.com>
2018-11-08 10:04:06 +0100

    m

---
+++ rule_based_automation.py
@@ -0,0 +1,151 @@
+import json
+import yaml
+import requests
+from requests.auth import HTTPBasicAuth
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+def add_function(file_name):
+    files = {'up_file': open('functions/' + file_name,'r')}
+    r=requests.post(url + '/files/helper-files/' + file_name, auth=HTTPBasicAuth(authuser, authpwd), headers={ 'Accept' : 'application/json' }, verify=False, files=files)
+    print 'loaded healthbot function ' + file_name
+#    return r.status_code
+
+def add_rule(file_name):
+    files = {'topics': open('rules/' + file_name,'r')}
+    r=requests.post(url + '/topics/', auth=HTTPBasicAuth(authuser, authpwd), headers={ 'Accept' : 'application/json'}, verify=False, files=files)
+    print 'loaded healthbot rule ' + file_name
+#    return r.status_code
+
+def add_playbook(file_name):
+    files = {'playbooks': open('playbooks/' + file_name,'r')}
+    r=requests.post(url + '/playbooks/', auth=HTTPBasicAuth(authuser, authpwd), headers={ 'Accept' : 'application/json' }, verify=False, files=files)
+    print 'loaded healthbot playbook ' + file_name 
+#    return r.status_code
+
+def add_device(dev):
+    payload=json.dumps(dev)
+    r = requests.post(url + '/device/' + dev['device-id'] + '/', auth=HTTPBasicAuth(authuser, authpwd), headers=headers, verify=False, data=payload)
+    print 'loaded healthbot configuration for the device: ' + dev['device-id']
+#    return r.status_code
+
+def add_device_group(group):
+    payload=json.dumps(group)
+    r = requests.post(url + '/device-group/' + group['device-group-name'] + '/', auth=HTTPBasicAuth(authuser, authpwd), headers=headers, verify=False, data=payload)
+    print 'loaded healthbot configuration for the device group: ' + group['device-group-name']
+#    return r.status_code
+
+def commit():
+    r = requests.post(url + '/configuration', auth=HTTPBasicAuth(authuser, authpwd), headers=headers, verify=False)
+    print 'healthbot configuration commited!'
+ #   return r.status_code
+
+
+# update this section with your healthbot ip address
+server = "10.209.18.74"
+authuser = "root"
+authpwd = "jfit"
+url = 'https://'+ server + ':8080/api/v1'
+headers = { 'Accept' : 'application/json', 'Content-Type' : 'application/json' }
+
+# update this section with your devices details
+devices = """[
+    {
+        "device-id": "abr1r",
+        "host": "10.221.129.147",
+        "open-config": {
+            "port": 32767
+        },
+        "authentication": {
+            "password": {
+                "username": "regress",
+                "password": "MaRtInI"
+            }
+        },
+        "variable" : [
+        {
+            "instance-id" : "enforce-int-state-instance-1",
+            "playbook" : "enforce-int-state",
+            "rule" : "interfaces/enforce-interfaces-state",
+            "variable-value" : [
+            {
+                "name" : "interface_name",
+                "value" : "ge-0/0/0"
+            }
+            ]
+        }
+        ]
+    },
+    {
+        "device-id": "abr2r",
+        "host": "10.221.129.144",
+        "open-config": {
+            "port": 32767
+        },
+        "authentication": {
+            "password": {
+                "username": "regress",
+                "password": "MaRtInI"
+            }
+        }
+    },
+    {
+        "device-id": "abr3r",
+        "host": "10.221.129.141",
+        "open-config": {
+            "port": 32767
+        },
+        "authentication": {
+            "password": {
+                "username": "regress",
+                "password": "MaRtInI"
+            }
+        },
+        "variable" : [
+        {
+            "instance-id" : "enforce-int-state-instance-1",
+            "playbook" : "enforce-int-state",
+            "rule" : "interfaces/enforce-interfaces-state",
+            "variable-value" : [
+            {
+                "name" : "interface_name",
+                "value" : "ge-*|xe-*"
+            }
+            ]
+        }
+        ]
+    }
+]"""
+
+# update this section with your device group details
+device_group = """{
+                "device-group-name" : "abr",
+                "description" : "abr",
+                "devices" : ["abr1r", "abr2r", "abr3r"],
+                "playbooks" : ["enforce-int-state"],
+                "variable" : [
+                {
+                    "instance-id" : "enforce-int-state-instance-1",
+                    "playbook" : "enforce-int-state",
+                    "rule" : "interfaces/enforce-interfaces-state"
+                }
+                ]
+            }"""
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+add_function('enable_a_disabled_interface.py')
+
+add_rule('enforce-interfaces-state.rule')
+
+add_playbook('enforce-int-state.playbook')
+
+my_devices=yaml.load(devices)
+for item in my_devices:
+    add_device(item)
+
+my_group=yaml.load(device_group)
+add_device_group(my_group)
+
+commit()
+
+
